481ab1606bcef26fa25076ac74a903b053d1c56e
masyapii <47060236+masyapii@users.noreply.github.com>
2019-11-05 00:39:38 +0700

    Update post.js

--- post.js
+++ post.js
@@ -18,7 +18,15 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://mampirsiniduluya.blogspot.com/2019/02/what-are-hipaa-laws.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+{var encryptedUrl=Base64.encode(a_to_vi);
+var x=Math.floor((Math.random()*6)+ 1);
+var xxx=null;
+if(x=="1"){xxx="2019/11/always-on-lookout-for-knowledge-source.html"}
+if(x=="2"){xxx="/2019/02/what-are-hipaa-laws.html"}
+if(x=="3"){xxx="2019/11/stay-legal-avoid-insurance-fraud.html"}
+if(x=="4"){xxx="/2019/02/benefits-of-buying-on-ebay.html"}
+if(x=="5"){xxx="2019/11/forex-trading-should-you-invest.html"}
+if(x=="6"){xxx="2019/11/forex-currency-trading.html"}a_to_ve[i].href="https://mampirsiniduluya.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
